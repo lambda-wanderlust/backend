9846057eac130799b0549c70928cf4cc8990d8ea
byronholmes2018 <lshopeful2018@mgmail.com>
2019-03-10 03:01:21 -0500

    Changed

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -6,8 +6,9 @@ const accountRoutes = require('./routes/accountRoutes')
 
 const router = express.Router()
 
-router.use('/users', userRoutes)
-router.use('/trips', tripRoutes)
-router.use('/accounts', accountRoutes)
+router.get('/', ()=>{console.log('server is here')})
+router.use('/api/users', userRoutes)
+router.use('/api/trips', tripRoutes)
+router.use('/api/accounts', accountRoutes)
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const apiRoutes = require('./api/apiRoutes')
 const server = express()
 
 
-server.use('/api', apiRoutes)
+server.use('/', apiRoutes)
 
 
 server.listen((process.env.PORT || 3000),()=>{console.log('API running on port')})
