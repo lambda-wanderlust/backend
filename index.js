118166c8213be86da8e8b1cf5a621d79ff769e9f
byronholmes2018 <lshopeful2018@mgmail.com>
2019-03-10 03:05:30 -0500

    Changed

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const apiRoutes = require('./api/apiRoutes')
 const server = express()
 
 
-server.get('/', apiRoutes)
+server.use('/api', apiRoutes)
+server.get('/', ()=>{console.log('server is here')})
 
 
 server.listen((process.env.PORT || 3000),()=>{console.log('API running on port')})
