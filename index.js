02671281dd96b47e3f073297de5a20db9a7eee4f
byronholmes2018 <lshopeful2018@mgmail.com>
2019-03-10 01:06:26 -0600

    Added route to server

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const server = express()
 
 server.use('/api', apiRoutes)
 
-server.get('/', (req,res)=>{
-  res.status(200).send("Here's server")
-})
+
 
 server.listen((process.env.PORT || 3000),()=>{console.log('API running on port')})
