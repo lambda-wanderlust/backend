8ba9a29e572ff22e352d5cc1e2f4a2eafc8b687f
byronholmes2018 <lshopeful2018@mgmail.com>
2019-03-11 05:37:18 -0500

    Made some spacing changes in index.js

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const express = require('express')
-
 const apiRoutes = require('./api/apiRoutes')
-
 const server = express()
-
 server.get('/', (req,res)=>{res.status(200).send("Here is the server")})
-
 server.use('/api', apiRoutes)
-
-
 server.listen((process.env.PORT || 3000),()=>{console.log('API running on port')})
