866cb3bae4a845c416027b82d69bb67ec83fb139
byronholmes2018 <lshopeful2018@mgmail.com>
2019-03-11 17:50:45 -0500

    Added import and use of cors

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const apiRoutes = require('./api/apiRoutes')
 const server = express()
+const cors = require('cors')
+
+server.use(cors())
 server.get('/', (req,res)=>{res.status(200).send("Here is the server")})
 server.use('/api', apiRoutes)
 server.listen((process.env.PORT || 3000),()=>{console.log('API running on port')})
