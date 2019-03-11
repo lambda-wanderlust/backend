2b8c475765fa9d1e8c21b5f5f66b0134dfe4df52
byronholmes2018 <lshopeful2018@mgmail.com>
2019-03-11 07:36:51 -0500

    changes

--- api/routes/accountRoutes.js
+++ api/routes/accountRoutes.js
@@ -3,9 +3,8 @@ const bcrypt = require('bcryptjs')
 const knex = require('knex')
 const db = require('../../data/dbConfig')
 const jwt = require('jsonwebtoken')
-const { authenticate } = require('../../auth/authenticate')
-const { checkRole } = require('../../auth/checkRole')
-const secret = process.env.JWT_SECRET
+const secret = process.env.JWT_SECRET ||     'add a .env file to root of project with the JWT_SECRET variable';
+
 const express = require('express')
 const router = express.Router()
 const express = require('express')
