beccb8a876b2eb2aa030238d99cf45deb559b4f1
byronholmes2018 <lshopeful2018@mgmail.com>
2019-03-14 11:39:49 -0500

    Restored protections on routes

--- api/routes/accountRoutes.js
+++ api/routes/accountRoutes.js
@@ -19,27 +19,27 @@ router.post('/login', login);
 
 
 
-router.get('/',  (req,res)=>{
+router.get('/', authenticate, (req,res)=>{
   db('users').where('role', 'tourist').then(users=>{res.status(200).json(users)}).catch(err=>{res.status(404).json({message:"No users found!"})})
 })
-router.get('/guides/',  (req,res)=>{
+router.get('/guides/', authenticate,  (req,res)=>{
   console.log('in guides')
   db('users').where('role', 'guide').then(users=>{res.status(200).json(users)}).catch(err=>{res.status(404).json({message:"No users found!"})})
 })
 
-router.get('/:id',  (req,res)=>{
+router.get('/:id', authenticate, (req,res)=>{
   const id = req.params.id
   db('users').where('id', id).where('role', 'tourist').first().then(users=>{res.status(200).json(users)}).catch(err=>{res.status(500).json({message:"Error trying to GET user!"})})
 })
 
 
-router.get('/guides/:id',  (req,res)=>{
+router.get('/guides/:id',authenticate,  (req,res)=>{
   const id = req.params.id
   db('users').where('id', id).where('role', 'guide').first().then(users=>{res.status(200).json(users)}).catch(err=>{res.status(500).json({message:"Error trying to GET user!"})})
 })
 
 ///
-router.put('/:id', (req,res)=>{
+router.put('/:id',  authenticate,(req,res)=>{
   console.log('in put')
   console.log(req.body)
   const id = req.params.id
